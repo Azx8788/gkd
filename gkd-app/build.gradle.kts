4cf192de0792e5cbe7a08c2a688793185ccab448
Azx8788 <Azx8788@users.noreply.github.com>
2026-09-14 17:00:49 +0000

    release: v2.0.0 (version bump + release workflow without play)

--- gkd-app/build.gradle.kts
+++ gkd-app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
     namespace = "li.gkd.app"
     defaultConfig {
         applicationId = "li.songe.gkd"
-        versionCode = 92
-        versionName = "1.12.1"
+        versionCode = 200
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
